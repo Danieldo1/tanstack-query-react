d4b7de2dd1455fd630004a4801e7a3c9d4b0e074
Dan <daniel.speranskiy@live.com>
2024-04-04 12:29:45 +0400

    added minor changes for styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -167,17 +167,14 @@ export default function Home() {
 
   const maxPrice = Math.max(sort.price.range[0], sort.price.range[1]);
 
-  console.log(sort);
   return (
-    <main
-      className={` mx-auto max-w-7xl px-4 sm:px-6 lg:px-8`}
-    >
+    <main className={` mx-auto max-w-7xl px-4 sm:px-6 lg:px-8`}>
       {isFilterSheetVisible && (
-        <div className="w-full h-full bg-black opacity-60 z-50 fixed top-0 left-0" />
+        <div onClick={toggleFilterSheet} className="w-full h-full bg-black opacity-60 z-50 fixed top-0 left-0" />
       )}
-      <div className="flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-          High quality selection
+      <div className="flex items-baseline justify-between border-b border-gray-200 pt-12 pb-6 sticky top-0 bg-white z-40">
+        <h1 className="text-5xl md:text-7xl text1 tracking-tight text-gray-900">
+          Shirt Shop
         </h1>
 
         <div className="flex items-center">
@@ -219,204 +216,35 @@ export default function Home() {
         </div>
       </div>
       <div className={`filter-sheet ${isFilterSheetVisible ? "open" : ""}`}>
-        <button onClick={toggleFilterSheet} className="close-button">
-          <XIcon />
+        <button onClick={toggleFilterSheet} className="absolute top-4 right-4">
+          <XIcon className="h-8 w-8" />
         </button>
-        <div className="filter-content ">
+        <div className="filter-content  ">
           <div
-            className={cn("lg:block", {
+            className={cn("lg:block pt-10 ", {
               block: isFilterSheetVisible,
               hidden: !isFilterSheetVisible,
             })}
           >
-            <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
-              {SUBCATEGORY.map((category) => (
-                <li key={category.name}>
-                  <button
-                    disabled={!category.selected}
-                    className="disabled:cursor-not-allowed disabled:opacity-50"
-                  >
-                    {category.name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-
-            <Accordion type="multiple" className="animate-none">
-              {/* COLOR FIlter */}
-              <AccordionItem value="color">
-                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
-                  <span className="text-gray-900font-medium">Color</span>
-                </AccordionTrigger>
-                <AccordionContent className="pt-6 animate-none ">
-                  <ul className="space-y-4">
-                    {COLOR_FILTER.options.map((color, idx) => (
-                      <li key={color.value} className="flex items-center ">
-                        <input
-                          type="checkbox"
-                          id={`color-${idx}`}
-                          className="h-4 w-4 rounded cursor-pointer border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                          onChange={() =>
-                            applyAllFilters({
-                              category: "color",
-                              value: color.value,
-                            })
-                          }
-                          checked={sort.color.includes(color.value)}
-                        />
-                        <label
-                          htmlFor={`color-${idx}`}
-                          className="ml-3 text-sm text-gray-600 cursor-pointer"
-                        >
-                          {color.label}
-                        </label>
-                      </li>
-                    ))}
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-              {/* SIZE FILTER */}
-              <AccordionItem value="size">
-                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
-                  <span className="text-gray-900font-medium">Size</span>
-                </AccordionTrigger>
-                <AccordionContent className="pt-6 animate-none ">
-                  <ul className="space-y-4">
-                    {SIZE_FILTER.options.map((size, idx) => (
-                      <li key={size.value} className="flex items-center ">
-                        <input
-                          type="checkbox"
-                          id={`size-${idx}`}
-                          className="h-4 w-4 rounded cursor-pointer border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                          onChange={() =>
-                            applyAllFilters({
-                              category: "size",
-                              value: size.value,
-                            })
-                          }
-                          checked={sort.size.includes(size.value)}
-                        />
-                        <label
-                          htmlFor={`size-${idx}`}
-                          className="ml-3 text-sm text-gray-600 cursor-pointer"
-                        >
-                          {size.label}
-                        </label>
-                      </li>
-                    ))}
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-              {/* PRICE FILTER */}
-              <AccordionItem value="price">
-                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
-                  <span className="text-gray-900font-medium">Price</span>
-                </AccordionTrigger>
-                <AccordionContent className="pt-6 animate-none ">
-                  <ul className="space-y-4">
-                    {PRICE_FILTER.options.map((size, idx) => (
-                      <li key={size.label} className="flex items-center ">
-                        <input
-                          type="radio"
-                          id={`price-${idx}`}
-                          className="h-4 w-4 rounded cursor-pointer border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                          onChange={() => {
-                            setSort((prev) => ({
-                              ...prev,
-                              price: {
-                                isCustom: false,
-                                range: [...size.value],
-                              },
-                            }));
-                            _debounceSubmit();
-                          }}
-                          checked={
-                            !sort.price.isCustom &&
-                            sort.price.range[0] === size.value[0] &&
-                            sort.price.range[1] === size.value[1]
-                          }
-                        />
-                        <label
-                          htmlFor={`price-${idx}`}
-                          className="ml-3 text-sm text-gray-600 cursor-pointer"
-                        >
-                          {size.label}
-                        </label>
-                      </li>
-                    ))}
-                    <li className="flex justify-center flex-col gap-2">
-                      <div>
-                        <input
-                          type="radio"
-                          id={`price-${PRICE_FILTER.options.length}`}
-                          className="h-4 w-4 rounded cursor-pointer border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                          onChange={() => {
-                            setSort((prev) => ({
-                              ...prev,
-                              price: {
-                                isCustom: true,
-                                range: [0, 100],
-                              },
-                            }));
-                            _debounceSubmit();
-                          }}
-                          checked={sort.price.isCustom}
-                        />
-                        <label
-                          htmlFor={`price-${PRICE_FILTER.options.length}`}
-                          className="ml-3 text-sm text-gray-600 cursor-pointer"
-                        >
-                          Custom
-                        </label>
-                      </div>
-                      <div className="flex justify-between">
-                        <p className="font-medium">Price</p>
-                        <div>
-                          {sort.price.isCustom
-                            ? minPrice.toFixed(0)
-                            : sort.price.range[0].toFixed(0)}{" "}
-                          $ -{" "}
-                          {sort.price.isCustom
-                            ? maxPrice.toFixed(0)
-                            : sort.price.range[1].toFixed(0)}{" "}
-                          $
-                        </div>
-                      </div>
-                      <Slider
-                        className={cn({
-                          "opacity-50": !sort.price.isCustom,
-                        })}
-                        disabled={!sort.price.isCustom}
-                        value={
-                          sort.price.isCustom ? sort.price.range : [0, 100]
-                        }
-                        min={0}
-                        defaultValue={[0, 100]}
-                        max={100}
-                        step={5}
-                        onValueChange={(range) => {
-                          const [newMin, newMax] = range;
-                          setSort((prev) => ({
-                            ...prev,
-                            price: {
-                              isCustom: true,
-                              range: [newMin, newMax],
-                            },
-                          }));
-                        }}
-                      />
-                    </li>
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
+            <Filters
+              COLOR_FILTER={COLOR_FILTER}
+              SIZE_FILTER={SIZE_FILTER}
+              PRICE_FILTER={PRICE_FILTER}
+              SUBCATEGORY={SUBCATEGORY}
+              applyAllFilters={applyAllFilters}
+              sort={sort}
+              setSort={setSort}
+              minPrice={minPrice}
+              maxPrice={maxPrice}
+              _debounceSubmit={_debounceSubmit}
+            />
           </div>
         </div>
       </div>
       <section className="pb-24 pt-6">
         <div className="grid grid-cols-1 gap-y-10 gap-x-6  lg:grid-cols-4  ">
           {/* Filters */}
-          <div className={`hidden lg:block`}>
+          <div className={`hidden lg:block  `}>
             <Filters
               COLOR_FILTER={COLOR_FILTER}
               SIZE_FILTER={SIZE_FILTER}
